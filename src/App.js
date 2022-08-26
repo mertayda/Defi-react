0c19d96206e3719c839916dd4048f0053be23f32
marneus <mertayda12@outlook.com>
2022-08-26 09:59:14 +0300

    Link

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Developers from './components/Developers';
+import Subscribe from './components/Subscribe';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <About></About>
+      <Developers></Developers>
+      <Subscribe></Subscribe>
+      <Footer></Footer>
+    </div>
+
+  );
+}
+
+export default App;
